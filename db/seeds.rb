b3f4358694be8c7791c6c0409ae0d834d96faba7
Indira Williams <indirajamurath@gmail.com>
2018-08-30 17:45:26 -0400

    migrated and seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{ name: 'Belle', email: 'belle@disney.com' },
+   { name: 'Adam', email: 'adam@disney.com' }])
+
+decks = Deck.create([{subject: 'Literature', user: users.first},
+  {subject: 'Geography', user: users.last}])
+
+Card.create([{front: 'Romeo & Juliet', back: 'William Shakespeare', deck: decks.first},
+  {front: 'To Kill A Mockingbird', back: 'Harper Lee', deck: decks.first},
+  {front: 'The Color Purple', back: 'Alice Walker', deck: decks.first},
+  {front: 'The Grapes of Wrath', back: 'John Steinbeck', deck: decks.first},
+  {front: 'Beloved', back: 'Toni Morrison', deck: decks.first},
+  {front: 'Dracula', back: 'Bram Stoker', deck: decks.first},
+  {front: 'Argentina', back: 'Buenos Aires', deck: decks.last},
+  {front: 'Malaysia', back: 'Kuala Lumpur', deck: decks.last},
+  {front: 'Syria', back: 'Damascus', deck: decks.last},
+  {front: 'Romania', back: 'Bucharest', deck: decks.last},
+  {front: 'Iceland', back: 'Reykjavik', deck: decks.last}])
